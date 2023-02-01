4e628c501fb52040ccb23215040c020ab23d7727
Antoine <antoine.gagniere@orolia2s.com>
2023-02-01 20:21:12 +0100

    Implement the string conversion of the SFRBX message

---
+++ src/ublox_header.c
@@ -0,0 +1,15 @@
+#include "ublox.h"
+
+#include <ft_string.h>
+
+#define append(S, ...) string_append_format(&S, __VA_ARGS__)
+
+t_string ublox_header_tostring(struct ublox_header* message)
+{
+	t_string result = NEW_STRING;
+
+	append(result, "class: %s, ", ublox_class_strings[message->class]);
+	append(result, "type: %#.2hhx, ", message->type);
+	append(result, "length: %hu", message->length);
+	return result;
+}
