08396947ae6c63c79765eeb654b8b8fae0f676e7
Antoine <antoine.gagniere@orolia2s.com>
2023-02-03 09:55:05 +0100

    Add another test to confirm the whole termios structure is correctly mapped

--- test/serial.c
+++ test/serial.c
@@ -6,6 +6,7 @@
 #include <termios.h>
 
 #include <stdbool.h>
+#include <string.h> // memcmp
 
 // clang-format off
 
@@ -67,6 +68,7 @@ TEST_SECTION(serial_input_modes, extract_name, INPUT_condition,
 	(is_utf8,                IUTF8)
 );
 
+
 #define LOCAL_condition(NAME, EXPECTED_VALUE)          \
     ({                                                 \
         struct serial_local_modes v = {. NAME = true}; \
@@ -81,6 +83,7 @@ TEST_SECTION(serial_local_modes, extract_name, LOCAL_condition,
 	(echo_kill,          ECHOK),
 	(enable_processing, IEXTEN)
 );
+
 
 #define OUTPUT_condition(NAME, EXPECTED_VALUE)          \
     ({                                                  \
@@ -100,6 +103,7 @@ TEST_SECTION(serial_output_modes, extract_name, OUTPUT_condition,
 	(vertical_tab_delay, VTDLY)
 );
 
+
 #define CHARS_condition(NAME, STD)          \
     ({                                      \
         serial_options_t opt = {};          \
@@ -117,18 +121,33 @@ TEST_SECTION(serial_control_characters, extract_name, CHARS_condition,
 	(minimum,     VMIN)
 );
 
+
+#define WHOLE_condition(NAME, MY_INIT, STD_INIT) \
+    ({                                           \
+        serial_options_t mine = { ID MY_INIT };  \
+        struct termios std = { ID STD_INIT };    \
+        memcmp(&mine, &std, sizeof(std)) == 0;   \
+    })
+
+TEST_SECTION(serial_whole, extract_name, WHOLE_condition,
+	(character_size, (.control.character_size = character_size_8), (.c_cflag = CS8 & CSIZE)),
+	(canonical,    (.local.canonical = true), (.c_lflag = ICANON)),
+	(input_speed,  (.input_speed  = serial_encode_baudrate(115200)), (.c_ispeed = B115200)),
+	(output_speed, (.output_speed = serial_encode_baudrate(115200)), (.c_ospeed = B115200))
+);
+
 // clang-format on
 
 static void print_end(int*)
 {
-	ft_printf("} %s// Section serial%s\n\n",
+	ft_printf("} %s// Section serial options%s\n\n",
 	          COLOR(BLUE), COLOR(NORMAL));
 }
 
-int test_serial()
+int test_serial_options()
 {
 	int __attribute__((cleanup(print_end))) a =
-		ft_printf("%sEntering section serial%s {\n\n",
+		ft_printf("%sEntering section serial options %s {\n\n",
 		          COLOR(BOLD, BLUE), COLOR(NORMAL));
 
 	return test_serial_types()
@@ -136,5 +155,6 @@ int test_serial()
 		|| test_serial_input_modes()
 		|| test_serial_local_modes()
 		|| test_serial_output_modes()
-		|| test_serial_control_characters();
+		|| test_serial_control_characters()
+		|| test_serial_whole();
 }
