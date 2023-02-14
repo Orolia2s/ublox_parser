255fc8a6bc58f2804d73c0f8a20f70691bc7824b
Antoine <antoine.gagniere@orolia2s.com>
2023-02-14 15:35:48 +0100

    Include the right header in tests

--- test/message_size.c
+++ test/message_size.c
@@ -1,4 +1,5 @@
 #include "ublox.h"
+#include "ublox_messages.h"
 
 #include <libunit.h>
 
@@ -8,10 +9,10 @@
 	sizeof(TYPE) == EXPECTED
 
 TEST_SECTION(messages_size, extract_name, MSGSZ_condition,
-	(ublox_message,  ublox_message_t,                                        4),
-	(ublox_checksum, ublox_checksum_t,                                       2),
-	(ubx_mon_rf,     struct ublox_monitoring_rf,   sizeof(ublox_message_t) + 4),
-	(ubx_mon_rf_block,     struct ublox_monitoring_rf_block, 24),
-	(ubx_mon_hw,     struct ublox_monitoring_hardware, sizeof(ublox_message_t) + 60),
-	(ubx_rxm_sfrbx,  struct ublox_navigation_data, sizeof(ublox_message_t) + 8)
+	(ublox_message,   ublox_message_t,                                             4),
+	(ublox_checksum,  ublox_checksum_t,                                            2),
+	(ubx_mon_rf,      struct ublox_monitoring_rf,        sizeof(ublox_message_t) + 4),
+	(ubx_mon_rf_block,struct ublox_monitoring_rf_block,                           24),
+	(ubx_mon_hw,      struct ublox_monitoring_hardware, sizeof(ublox_message_t) + 60),
+	(ubx_rxm_sfrbx,   struct ublox_navigation_data,      sizeof(ublox_message_t) + 8)
 )
