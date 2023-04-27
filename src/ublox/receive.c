862871da1ab7cc1846886ada1eb49465f14dfbf7
Antoine <antoine.gagniere@orolia2s.com>
2023-04-27 15:23:50 +0200

    Flush vector when discarding message

--- src/ublox/receive.c
+++ src/ublox/receive.c
@@ -11,17 +11,6 @@
 
 #include <stdlib.h>
 
-char* byte_to_string(unsigned char* byte)
-{
-	char  half[] = "0123456789abcdef";
-	char* ans    = malloc(3);
-
-	ans[0] = half[*byte >> 4];
-	ans[1] = half[*byte % 16];
-	ans[2] = 0;
-	return ans;
-}
-
 static void ftq_pop_front_into_array(t_deque* queue, t_array* array, size_t count)
 {
 	fta_reserve(array, count);
@@ -41,14 +30,14 @@ sync:
 		FTQ_POP_FRONT_ONE(queue, NULL);
 	}
 	if (ftq_is_empty(queue) || *(uint8_t*)ftq_first(queue) != ublox_sync_chars[0])
-		return NULL;
+		goto fail;
 	FTQ_POP_FRONT_ONE(queue, NULL);
 	if (*(uint8_t*)ftq_first(queue) != ublox_sync_chars[1])
 		goto sync;
 	FTQ_POP_FRONT_ONE(queue, NULL);
 	if (!is_valid_ublox_class(*(uint8_t*)ftq_first(queue)))
 	{
-		log_trace("Discaring message of unknown class %#.2hhx", *(uint8_t*)ftq_first(queue));
+		log_trace("Discaring header of unknown class %#.2hhx", *(uint8_t*)ftq_first(queue));
 		goto sync;
 	}
 	ftq_pop_front_into_array(queue, result, sizeof(struct ublox_header));
@@ -58,28 +47,26 @@ sync:
 	{
 		log_trace("Unable to read completely a message of class %s, discarding header",
 		          cstring_from_ublox_class(message[0]->ublox_class));
-		goto fail;
+		ft_print_memory(result->data, result->size);
+		result->size = 0;
+		goto sync;
 	}
 	ftq_pop_front_into_array(queue, result, message[0]->length);
-	fta_trim(result);
 	{
 		ublox_checksum_t computed = ublox_compute_checksum(result->data, result->size);
 		ublox_checksum_t expected;
+
 		ftq_pop_front(queue, &expected, sizeof(expected));
 		if (*(uint16_t*)&computed != *(uint16_t*)&expected)
 		{
-			log_warn("Discarding message of class %s and type %#.2hhx with invalid checksum {%s}, flushing buffer",
-			         cstring_from_ublox_class(message[0]->ublox_class),
-			         message[0]->type,
-			         ublox_header_tostring(message[0]));
-			char discard[ftq_size(queue)];
-			char* str = ftq_string(queue, byte_to_string);
-			ftq_pop_front(queue, discard, ftq_size(queue));
-			log_info("Discarding: %s", str);
-			free(str);
+			RAII(t_string) str = ublox_header_tostring(message[0]);
+			log_warn("Discarding message with invalid checksum: {%s}", cstring(&str));
+			ft_print_memory(result->data, result->size);
+			result->size = 0;
 			goto sync;
 		}
 	}
+	fta_trim(result);
 	return result->data;
 fail:
 	fta_clear(result);
